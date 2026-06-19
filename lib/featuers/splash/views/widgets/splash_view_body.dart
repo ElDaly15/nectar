2d7d99868fcc8d32f7dfc04f45eacd715b545251
Daly15_ <dalydevelopment15@gmail.com>
2026-06-19 16:28:01 +0300

    refactor: change SplashViewBody to StatefulWidget and implement animations

--- lib/featuers/splash/views/widgets/splash_view_body.dart
+++ lib/featuers/splash/views/widgets/splash_view_body.dart
@@ -1,10 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nectar/core/utils/app_images.dart';
+// import 'package:get/get.dart' as g;
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 0.86).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.slowMiddle,
+      ),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    _animationController.forward();
+
+    Future.delayed(
+        const Duration(milliseconds: 3500),
+        // () => FirebaseAuth.instance.currentUser != null
+        //     ? g.Get.to(() => const HomeView(),
+        //         transition: g.Transition.fade,
+        //         duration: const Duration(milliseconds: 800))
+        //     : g.Get.off(() => const GetStartedView(),
+        //         transition: g.Transition.fade,
+        //         duration: const Duration(milliseconds: 800)));
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              return FadeTransition(
+                opacity: _fadeAnimation,
+                child: Transform.scale(
+                  scale: _scaleAnimation.value,
+                  child: child,
+                ),
+              );
+            },
+            child: SvgPicture.asset(Assets.imagesLogo),
+          ),
+        ),
+      ],
+    );
   }
 }
