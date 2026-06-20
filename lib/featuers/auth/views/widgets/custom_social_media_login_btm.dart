e68341a743af2d4f682b3d553512b32d14b5849a
Daly15_ <dalydevelopment15@gmail.com>
2026-06-20 12:04:05 +0300

    feat(auth): add CustomSocialMediaLoginBtm widget and integrate into MainAuthColumnData

---
+++ lib/featuers/auth/views/widgets/custom_social_media_login_btm.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:nectar/core/utils/app_colors.dart';
+import 'package:nectar/core/utils/app_styles.dart';
+
+class CustomSocialMediaLoginBtm extends StatelessWidget {
+  const CustomSocialMediaLoginBtm({super.key, required this.text, required this.image, required this.onPressed, required this.color});
+  final String text , image;
+  final VoidCallback onPressed;
+  final Color color;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: color,
+              minimumSize: Size(double.infinity, 67),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            onPressed: onPressed,
+            child: Row(
+              children: [
+                SvgPicture.asset(image, width: 22, height: 24),
+                Spacer(),
+                Text(text, style: TextStyles.font24SemiBold(context).copyWith(color: AppColors.whiteColor)),
+                Spacer(),
+              ],
+            ),
+          );
+  }
+}
--- lib/featuers/auth/views/widgets/main_auth_column_data.dart
+++ lib/featuers/auth/views/widgets/main_auth_column_data.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nectar/core/utils/app_colors.dart';
+import 'package:nectar/core/utils/app_images.dart';
 import 'package:nectar/core/utils/app_styles.dart';
+import 'package:nectar/featuers/auth/views/widgets/custom_social_media_login_btm.dart';
 import 'package:nectar/featuers/auth/views/widgets/row_of_phone_auth.dart';
 
 class MainAuthColumnData extends StatelessWidget {
@@ -7,26 +11,46 @@ class MainAuthColumnData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Get your groceries\nwith nectar',
-                style: TextStyles.font24SemiBold(
-                  context,
-                ).copyWith(fontSize: 25),
-              ),
-              const SizedBox(height: 30),
-             RowOfPhoneAuth(),
-              const SizedBox(height: 4),
-              Divider(
-                thickness: 1,
-                color: Color(0xffE2E2E2),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Get your groceries\nwith nectar',
+            style: TextStyles.font24SemiBold(context).copyWith(fontSize: 25),
           ),
-        );
+          const SizedBox(height: 30),
+          RowOfPhoneAuth(),
+          const SizedBox(height: 4),
+          Divider(thickness: 1, color: Color(0xffE2E2E2)),
+
+          const SizedBox(height: 30),
+          Center(
+            child: Text(
+              'Or connect with social media',
+              style: TextStyles.font18SemiBold(
+                context,
+              ).copyWith(color: AppColors.greyText),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          CustomSocialMediaLoginBtm(
+            text: 'Connect with Google',
+            image: Assets.imagesGoogle,
+            onPressed: () {},
+            color: Color(0xff4285F4),
+          ),
+          const SizedBox(height: 20),
+          CustomSocialMediaLoginBtm(
+            text: 'Connect with Facebook',
+            image: Assets.imagesFacebook,
+            onPressed: () {},
+            color: Color(0xff3B5998),
+          ),
+        ],
+      ),
+    );
   }
 }
